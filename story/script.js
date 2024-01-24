53b8e5eb5b48e29212a76bba4fe09a666289fa2e
Rahul Saravanabavan <142858065+rahulsaravanabavan@users.noreply.github.com>
2024-01-24 10:08:13 -0500

    Add files via upload

---
+++ story/script.js
@@ -0,0 +1,7 @@
+// Map options
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 44.35411847409072, lng: -78.7409030701281},
+        zoom: 5,
+    });
+}
